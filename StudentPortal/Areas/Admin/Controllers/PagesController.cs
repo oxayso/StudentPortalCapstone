4e16830ca8eecb8e7bdc6bf319c21e8a77acc304
Olivia Xaysombath <Olivia Xaysombath>
2017-03-16 11:04:56 -0500

    added functionality for admin to manage user nav bar

---
+++ StudentPortal/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentPortal.Models.Data;
+using StudentPortal.Models.ViewModels.Pages;
+
+namespace StudentPortal.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pagesList;
+
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pagesList);
+        }
+
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string root;
+
+                PageDTO dto = new PageDTO();
+
+                dto.Title = model.Title;
+
+                if (string.IsNullOrWhiteSpace(model.Root))
+                {
+                    root = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    root = model.Root.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Root == root))
+                {
+                    ModelState.AddModelError("", "The title you entered already exists.");
+                    return View(model);
+                }
+
+                dto.Root = root;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Successfully added a new page!";
+
+            return RedirectToAction("AddPage");
+        }
+
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                model = new PageVM(dto);
+            }
+
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                int id = model.Id;
+
+                string root = "home";
+
+                PageDTO dto = db.Pages.Find(id);
+
+                dto.Title = model.Title;
+
+                if (model.Root != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Root))
+                    {
+                        root = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        root = model.Root.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) ||
+                     db.Pages.Where(x => x.Id != id).Any(x => x.Root == root))
+                {
+                    ModelState.AddModelError("", "The title or root you entered already exists.");
+                    return View(model);
+                }
+
+                dto.Root = root;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Successfully edited the page!";
+
+            return RedirectToAction("EditPage");
+        }
+    }
+}
---
+++ StudentPortal/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentPortal.Models.Data;
+
+namespace StudentPortal.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Root = row.Root;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Root { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+
+        [AllowHtml]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
