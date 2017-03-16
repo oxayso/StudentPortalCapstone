3f494870a310e630561ce6603bf7c1d1ada41fed
Olivia Xaysombath <Olivia Xaysombath>
2017-03-16 14:28:11 -0500

    admin has the ability to add new cohorts

--- StudentPortal/Areas/Admin/Controllers/StudentsController.cs
+++ StudentPortal/Areas/Admin/Controllers/StudentsController.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StudentPortal.Models.Data;
+using StudentPortal.Models.ViewModels.Students;
 
 namespace StudentPortal.Areas.Admin.Controllers
 {
     public class StudentsController : Controller
     {
-        // GET: Admin/Students
-        public ActionResult Index()
+        public ActionResult Cohorts()
         {
-            return View();
+            List<CohortVM> cohortVMList;
+
+            using (Db db = new Db())
+            {
+                cohortVMList = db.Cohort
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CohortVM(x))
+                    .ToList();
+            }
+
+            return View(cohortVMList);
+        }
+
+        [HttpPost]
+        public string AddNewCohort(string catName)
+        {
+            string id;
+
+            using (Db db = new Db())
+            {
+                if (db.Cohort.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                CohortDTO dto = new CohortDTO();
+
+                dto.Name = catName;
+                dto.Root = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                db.Cohort.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+            }
+
+            return id;
         }
     }
+
 }
